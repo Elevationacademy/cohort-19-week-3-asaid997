86abd21076a92df2057510515b9240aa9a57375e
VelZac <androwas101@gmail.com>
2020-11-03 20:06:48 +0200

    finished all ex

--- js-closure-modules/ex/main.js
+++ js-closure-modules/ex/main.js
@@ -38,3 +38,24 @@ const bank = Bank()
 bank.deposit(200)
 bank.deposit(250)
 bank.showBalance() //should print 950
+
+
+//3 challenging
+const SongsManager = function(){
+    const songs = {}
+
+    const addSong = (name,url) => songs[name] = url.split('=')[1]
+    const getSong = (name) => "https://www.youtube.com/watch?v="+songs[name]
+
+    return{
+        addSong,
+        getSong
+    }  
+}
+
+const songsManager = SongsManager()
+songsManager.addSong("sax", "https://www.youtube.com/watch?v=3JZ4pnNtyxQ")
+songsManager.addSong("how long", "https://www.youtube.com/watch?v=CwfoyVa980U")
+songsManager.addSong("ain't me", "https://www.youtube.com/watch?v=D5drYkLiLI8")
+
+console.log(songsManager.getSong("how long")) // should print https://www.youtube.com/watch?v=3JZ4pnNtyxQ
